51c2d7082c16d08eb9b0c30319a7f11506ecfbc1
Tushar Dukane <117209869+Tu5harD@users.noreply.github.com>
2023-09-30 11:19:27 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,16 @@ export default function Home({ products }) {
             <Wrapper>
                 {/* heading and paragaph start */}
                 <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
-                    <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-                        Cushioning for Your Miles
-                    </div>
-                    <div className="text-md md:text-xl">
-                        A lightweight Nike ZoomX midsole is combined with
-                        increased stack heights to help provide cushioning
-                        during extended stretches of running.
-                    </div>
-                </div>
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Unleash the Power of Mobile Technology
+          </div>
+          <div className="text-md md:text-xl">
+            Welcome to Online Mobile Store, where cutting-edge technology meets
+            unbeatable prices. Explore our curated collection of smartphones and
+            accessories to find the perfect mobile solution for your lifestyle
+          </div>
+          
+        </div>
                 {/* heading and paragaph end */}
 
                 {/* products grid start */}
